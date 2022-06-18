e0381409113f5c0cc0da54947925063e7f826be2
7r0n1x <techsoftec.tronix@gmail.com>
2022-06-18 18:33:38 -0500

    Configured port

--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const app = express();
 
+app.set('port', process.env.PORT || 4000);
+
 app.use(morgan('dev'));
 
 module.exports = app;
